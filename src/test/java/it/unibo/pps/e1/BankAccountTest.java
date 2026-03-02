68addeef1db81e849fc6215a89c83f46ea735dfa
Elisa Yan <elisa.yan@studio.unibo.it>
2026-03-02 11:47:34 +0100

    refactor: make BankAccountTest in abstract

--- src/test/java/it/unibo/pps/e1/BankAccountTest.java
+++ src/test/java/it/unibo/pps/e1/BankAccountTest.java
@@ -1,20 +1,19 @@
 package it.unibo.pps.e1;
 
-import it.unibo.pps.e1.BankAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class BankAccountTest {
+public abstract class BankAccountTest {
 
-    private BankAccount account;
+    protected BankAccount account;
+
+    protected abstract BankAccount createBankAccount();
 
     @BeforeEach
-    void init(){
-        CoreBankAccount core = new CoreBankAccount();
-        this.account = new SilverBankAccount(core);
+    public void setUp() {
+        this.account = createBankAccount();
     }
 
     @Test
@@ -28,17 +27,11 @@ public class BankAccountTest {
         assertEquals(1000, this.account.getBalance());
     }
 
-    @Test
-    public void testCanWithdraw() {
-        this.account.deposit(1000);
-        this.account.withdraw(200);
-        assertEquals(799, this.account.getBalance());
-    }
-
-    @Test
-    public void testCannotWithdrawMoreThanAvailable(){
-        this.account.deposit(1000);
-        assertThrows(IllegalStateException.class, () -> this.account.withdraw(1200));
-    }
-
+//    @Test
+//    public void testCanWithdraw() {
+//        this.account.deposit(1000);
+//        this.account.withdraw(500);
+//
+//        assertEquals(500, this.account.getBalance());
+//    }
 }
