7f6205e2913a0a8456bc091a684720e7e37039e8
Elisa Yan <elisa.yan@studio.unibo.it>
2026-03-04 17:56:49 +0100

    refactor: delegate board and knight responsibilities to Board and Knight classes

--- src/main/java/it/unibo/pps/e2/LogicsImpl.java
+++ src/main/java/it/unibo/pps/e2/LogicsImpl.java
@@ -1,61 +1,51 @@
 package it.unibo.pps.e2;
 
-import java.util.*;
+import java.util.Random;
 
 public class LogicsImpl implements Logics {
 
     private final Pair<Integer, Integer> pawn;
-    private Pair<Integer, Integer> knight;
+    private final Knight knight;
+    private final Board board;
     private final Random random = new Random();
-    private final int size;
-    private final MoveValidator moveValidator;
 
     public LogicsImpl(int size) {
-        this(size, new MoveValidatorImpl());
+        MoveValidator validator = new MoveValidatorImpl();
+        this.board = new BoardImpl(size);
+        this.pawn = randomEmptyPosition();
+        this.knight = new KnightImpl(randomEmptyPosition(), validator);
     }
 
-    public LogicsImpl(int size,
+    public LogicsImpl(Board board,
                       Pair<Integer, Integer> pawn,
-                      Pair<Integer, Integer> knight) {
-        this(size, pawn, knight, new MoveValidatorImpl());
-    }
-
-    public LogicsImpl(int size, MoveValidator moveValidator) {
-        this.size = size;
-        this.moveValidator = moveValidator;
-        this.pawn = this.randomEmptyPosition();
-        this.knight = this.randomEmptyPosition();
-    }
-
-    public LogicsImpl(int size, Pair<Integer, Integer> pawn, Pair<Integer, Integer> knight, MoveValidator moveValidator) {
-        this.size = size;
+                      Pair<Integer, Integer> knightPos,
+                      MoveValidator validator) {
+        this.board = board;
         this.pawn = pawn;
-        this.knight = knight;
-        this.moveValidator = moveValidator;
+        this.knight = new KnightImpl(knightPos, validator);
     }
 
-    private final Pair<Integer, Integer> randomEmptyPosition() {
-        Pair<Integer, Integer> pos = new Pair<>(this.random.nextInt(size), this.random.nextInt(size));
-        // the recursive call below prevents clash with an existing pawn
-        return this.pawn != null && this.pawn.equals(pos) ? randomEmptyPosition() : pos;
+    private Pair<Integer, Integer> randomEmptyPosition() {
+        Pair<Integer, Integer> pos =
+                new Pair<>(this.random.nextInt(this.board.getSize()), this.random.nextInt(this.board.getSize()));
+        return this.pawn != null && this.pawn.equals(pos)
+                ? randomEmptyPosition()
+                : pos;
     }
 
     @Override
     public boolean hit(int row, int col) {
-        if (row < 0 || col < 0 || row >= this.size || col >= this.size) {
-            throw new IndexOutOfBoundsException();
-        }
-        // Below a compact way to express allowed moves for the knight
-        if (moveValidator.isValidMove(this.knight, new Pair<>(row, col))) {
-            this.knight = new Pair<>(row, col);
-            return this.pawn.equals(this.knight);
+        this.board.checkBounds(row, col);
+
+        if (this.knight.moveTo(new Pair<>(row, col))) {
+            return this.pawn.equals(this.knight.getPosition());
         }
         return false;
     }
 
     @Override
     public boolean hasKnight(int row, int col) {
-        return this.knight.equals(new Pair<>(row, col));
+        return this.knight.getPosition().equals(new Pair<>(row, col));
     }
 
     @Override
