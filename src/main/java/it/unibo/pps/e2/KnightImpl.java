cc3d4605713b4b2da5577169830327740443559c
Elisa Yan <elisa.yan@studio.unibo.it>
2026-03-04 17:53:23 +0100

    reformat code

--- src/main/java/it/unibo/pps/e2/KnightImpl.java
+++ src/main/java/it/unibo/pps/e2/KnightImpl.java
@@ -2,24 +2,25 @@ package it.unibo.pps.e2;
 
 public class KnightImpl implements Knight {
 
-    private Pair<Integer,Integer> position;
+    private Pair<Integer, Integer> position;
     private final MoveValidator validator;
 
-    public KnightImpl(Pair<Integer,Integer> start,
-                  MoveValidator validator) {
+    public KnightImpl(Pair<Integer, Integer> start,
+                      MoveValidator validator) {
         this.position = start;
         this.validator = validator;
     }
 
+
     @Override
-    public Pair<Integer,Integer> getPosition() {
+    public Pair<Integer, Integer> getPosition() {
         return this.position;
     }
 
     @Override
     public boolean moveTo(Pair<Integer, Integer> target) {
-        if (validator.isValidMove(position, target)) {
-            position = target;
+        if (this.validator.isValidMove(this.position, target)) {
+            this.position = target;
             return true;
         }
         return false;
